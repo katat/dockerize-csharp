3e844211f46937d4965c3f294f57b57f34205b9c
katat <kata.choi@gmail.com>
2016-04-16 17:41:53 +0800

    init

---
+++ dockerize-csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dockerizecsharp
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("What is your name?");
+			var name = Console.ReadLine ();
+			Console.WriteLine ("Hello " + name);
+		}
+	}
+}
